c91db647bac1234d71dbdee376aab951117e6e09
msniresh <msniresh2000@gmail.com>
2025-01-04 19:19:13 +0530

    Methods 4,5,6 is added newly after import

--- src/test/java/git/task1/GitTask1.java
+++ src/test/java/git/task1/GitTask1.java
@@ -13,6 +13,21 @@ public class GitTask1 {
 	}
 	private void method3() {
 		 System.out.println("method3");
+		 
+		 
+
+	}
+	
+	private void method4() {
+		 System.out.println("method4");
+
+	}
+	private void method5() {
+		 System.out.println("method5");
+
+	}
+	private void method6() {
+		 System.out.println("method6");
 
 	}
 }
