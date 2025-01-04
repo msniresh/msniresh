60ab8100ee0ad7815fc43a99b6dcd6a1d03fbfc7
msniresh <msniresh2000@gmail.com>
2025-01-04 18:57:23 +0530

    Three methods are modified

--- src/test/java/git/task1/GitTask1.java
+++ src/test/java/git/task1/GitTask1.java
@@ -2,4 +2,17 @@ package git.task1;
 
 public class GitTask1 {
 
+	
+	private void method1() {
+	     System.out.println("method1");
+
+	}
+	private void method2() {
+		 System.out.println("method2");
+
+	}
+	private void method3() {
+		 System.out.println("method3");
+
+	}
 }
